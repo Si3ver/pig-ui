93c848ffabd9f73bcb80aac1409d46f42e14734d
Si3ver <izhouwl@163.com>
2024-05-26 21:28:37 +0800

    feat: 首页 splitpanes 做布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,30 +1,36 @@
 <template>
-  <div class="layout-padding">
-    <div class="layout-padding-auto layout-padding-view">
-      <splitpanes>
-        <pane size="70">
-          <splitpanes horizontal>
-            <pane size="25">
-              <current-user/>
-            </pane>
-            <pane size="75">
-              <favorite/>
-            </pane>
-          </splitpanes>
-        </pane>
-        <pane size="30">
-          <splitpanes horizontal>
-            <pane size="58">
-              <schedule/>
-            </pane>
-            <pane size="42">
-              <sys-log/>
-            </pane>
-          </splitpanes>
-        </pane>
-      </splitpanes>
-    </div>
-  </div>
+	<div class="layout-padding">
+		<div class="layout-padding-auto layout-padding-view">
+			<splitpanes>
+				<!-- 左边 70% -->
+				<pane size="70">
+					<splitpanes horizontal>
+						<!-- 左上 25%：当前用户 -->
+						<pane size="25">
+							<current-user />
+						</pane>
+						<!-- 左下 75%：快捷导航栏 -->
+						<pane size="75">
+							<favorite />
+						</pane>
+					</splitpanes>
+				</pane>
+				<!-- 右边 30% -->
+				<pane size="30">
+					<splitpanes horizontal>
+						<!-- 右上 58%：日历 -->
+						<pane size="58">
+							<schedule />
+						</pane>
+						<!-- 右下 42%：系统日志 -->
+						<pane size="42">
+							<sys-log />
+						</pane>
+					</splitpanes>
+				</pane>
+			</splitpanes>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts" name="home">
