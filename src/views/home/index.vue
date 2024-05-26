ac805fc58c05fef4e69aa7cb359f701df5ed716b
Si3ver <izhouwl@163.com>
2024-05-26 23:53:33 +0800

    feat: 首页 tailwind 做布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,41 +1,26 @@
 <template>
 	<div class="layout-padding">
 		<div class="layout-padding-auto layout-padding-view">
-			<splitpanes>
-				<!-- 左边 70% -->
-				<pane size="70">
-					<splitpanes horizontal>
-						<!-- 左上 25%：当前用户 -->
-						<pane size="25">
-							<current-user />
-						</pane>
-						<!-- 左下 75%：快捷导航栏 -->
-						<pane size="75">
-							<favorite />
-						</pane>
-					</splitpanes>
-				</pane>
-				<!-- 右边 30% -->
-				<pane size="30">
-					<splitpanes horizontal>
-						<!-- 右上 58%：日历 -->
-						<pane size="58">
-							<schedule />
-						</pane>
-						<!-- 右下 42%：系统日志 -->
-						<pane size="42">
-							<sys-log />
-						</pane>
-					</splitpanes>
-				</pane>
-			</splitpanes>
+			<div class="header flex justify-center items-center">
+				<img class="w-50px h-100px mr-10" src="../../assets/logo-mini.svg" alt="logo" />
+				<span class="text-6xl font-medium">xxx管理系统</span>
+			</div>
+			<div class="flex flex-row">
+				<div class="basis-1/3 p-2 text-2xl text-justify">
+					<div class="indent-8">
+						豫章故郡，洪都新府2。星分翼轸，地接衡庐3。襟三江而带五湖4，控蛮荆而引瓯越5。物华天宝，龙光射牛斗之墟6；人杰地灵，徐孺下陈蕃之榻
+					</div>
+					<div class="indent-8">
+						雄州雾列8，俊采星驰9。台隍枕夷夏之交10，宾主尽东南之美11。都督阎公之雅望，棨戟遥临；宇文新州之懿范，襜帷暂驻12。十旬休假13，胜友如云；千里逢迎14，高朋满座。腾蛟起凤，孟学士之词宗15；紫电青霜，王将军之武库16。家君作宰，路出名区；童子何知，躬逢胜饯17。
+						时维九月，序属三秋18。潦水尽而寒潭清，烟光凝而暮山紫。俨骖騑（𬴂）68于上路19，访风景于崇阿。临帝子之长洲，得天人之旧馆20。层峦耸翠，上出重霄；飞阁流丹，下临无地21。鹤汀凫渚，穷岛屿之萦回；桂殿兰宫，即冈峦之体势22。
+					</div>
+				</div>
+				<div class="basis-2/3 pt-4 pl-4">
+					<img class="w-full" src="../../assets/logo-mini.svg" alt="logo" />
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
-<script setup lang="ts" name="home">
-const CurrentUser = defineAsyncComponent(() => import('./current-user.vue'));
-const Favorite = defineAsyncComponent(() => import('./favorite.vue'));
-const Schedule = defineAsyncComponent(() => import('./schedule.vue'));
-const SysLog = defineAsyncComponent(() => import('./sys-log.vue'));
-</script>
+<script setup lang="ts" name="home"></script>
