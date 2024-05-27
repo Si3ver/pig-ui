46a37837ea199f61618b2ae2647dbd8a1d816c37
Si3ver <izhouwl@163.com>
2024-05-27 22:45:55 +0800

    feat: init ifrTest

---
+++ src/views/ifrTest/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="layout-padding">
+		<div class="layout-padding-auto layout-padding-view">
+			<div class="layout-">111</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts" name="ifrTest"></script>
+
+<style scoped lang="scss"></style>
