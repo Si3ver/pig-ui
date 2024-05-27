777be932ddbe6fa15188c94183f09dd16c8349ec
Si3ver <izhouwl@163.com>
2024-05-27 22:50:32 +0800

    feat: update ifrTest

--- src/views/ifrTest/index.vue
+++ src/views/ifrTest/index.vue
@@ -1,11 +1,13 @@
 <template>
 	<div class="layout-padding">
 		<div class="layout-padding-auto layout-padding-view">
-			<div class="layout-">111</div>
+			<iframe :src="url" width="100%" height="100%"></iframe>
 		</div>
 	</div>
 </template>
 
-<script setup lang="ts" name="ifrTest"></script>
+<script setup lang="ts" name="ifrTest">
+const url = 'http://localhost:8888';
+</script>
 
 <style scoped lang="scss"></style>
